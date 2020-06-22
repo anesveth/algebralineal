c7f4d10201921abdbdbc9e525920226ef6ecd4cd
AndreaNathalia <49224603+AndreaNathalia@users.noreply.github.com>
2020-06-21 22:05:38 -0600

    add encode function

--- codificar.py
+++ codificar.py
@@ -16,10 +16,6 @@ key=np.array([[1,2,1,],
                [2,1,0]])
 keysize=len(key)
 
-## .upper por que el diccionario esta todo en mayusculas y hace mas facil la comparacion.
-omessage=(input("Ingrese el mensaje a codificar: ")).upper()
-
-
 ## crea una matriz con el mismo numero de columnas que la matriz clave
 def tomatriz(omessage, keysize): 
     message_matriz=np.zeros((keysize,keysize))
@@ -40,12 +36,40 @@ def tomatriz(omessage, keysize):
     message_matriz=np.array(row)
     return message_matriz
 
+
+## matriz fila para guardar temporalmente cada fila del mensaje ingresado
+temporalRow = []
+
+## matriz que guarda el mensaje encriptado
+encodeMessage = []
+
 def encode(message_matriz,key):
-    pass
+    ## for para recorrer la matriz con el mensaje ingresado y encriptarlo 
+    for row in range(len(message_matriz)):
+        temporalRow = message_matriz[row]
+        encodeMessage.append(np.matmul(temporalRow, key))
+        temporalRow = []
 
-print(tomatriz(omessage,keysize))
+def prettyPrint(encodeMessage):
+    encodeMessageStr = ""
+    toString = str(encodeMessage)
 
 
+## main
+def main():
+    print("\n\n--------------- PROYECTO CRIPTOGRAFÍA ---------------")
 
-# if __name__ == "__main__":
-#     main()
+    ## Pedir el mensaje
+    ## .upper por que el diccionario esta todo en mayusculas y hace mas facil la comparacion.
+    omessage=(input("\n--> Ingrese el mensaje a codificar: ")).upper()
+    ToMatrix = tomatriz(omessage,keysize)
+
+    ## Encriptar mensaje
+    encode(ToMatrix, key)
+
+    ## Imprimir el mensaje encriptado
+    print(str(encodeMessage), end=' ')
+
+
+if __name__ == "__main__":
+    main()
