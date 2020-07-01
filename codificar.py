b81de517465a1d7fa927666e99dcd78d9694a776
katherineggs <katherinegarcia@ufm.edu>
2020-06-30 18:04:31 -0600

    option 3 not done

--- codificar.py
+++ codificar.py
@@ -1,6 +1,4 @@
-import string
-import math
-import random
+import string, math, random
 import numpy as np
 import sympy as sp
 
@@ -10,13 +8,13 @@ diccionario.insert(14,"Ñ")
 diccionario.insert(0," ")
 diccionario.append(".")
 
-key=np.array([[1,2,1,],
-                [0,-1,3],
-                [2,1,0]])
-keysize = len(key)
-# keysize = random.randint(3,5)
-# keyValue = random.randint(3,5)
-# key = np.random.randint((keyValue), size=(keysize, keysize))
+# key=np.array([[1,2,1,],
+#                 [0,-1,3],
+#                 [2,1,0]])
+# keysize = len(key)
+keysize = random.randint(3,5)
+keyValue = random.randint(3,5)
+key = np.random.randint((keyValue), size=(keysize, keysize))
 
 ## crea una matriz con el mismo numero de columnas que la matriz clave
 def toMatriz(omessage, keysize): 
@@ -49,6 +47,7 @@ def encode(message_matriz,key):
         temporalRow = message_matriz[row]
         encodeMessage.append(np.matmul(temporalRow, key))
         temporalRow = []
+    return encodeMessage
 
 def toDictionary(encodeMessage):
     for i in encodeMessage:
@@ -66,9 +65,10 @@ def toMatrix(message, keysize):
     mess = list(map(int, message.split()))
     message_matriz=np.zeros((math.ceil(len(mess)/keysize),keysize))
     row=[]
+    rows = 0
     k=0
     ## math.ceil es para round up la division
-    while k < len(message):
+    while rows < (math.ceil(len(mess)/keysize)):
         subrow=[]
         for j in range(keysize):    
             try:
@@ -80,11 +80,12 @@ def toMatrix(message, keysize):
         k += keysize
         row.append(subrow)
         message_matriz=np.array(row)
+        rows += 1
     return message_matriz
 
 def decode(message, keyInv):
-    # keyInvL = len(keyInv)
-    result = np.matmul(message,keyInv)
+    invKey = np.linalg.inv(keyInv)
+    result = np.matmul(message,invKey)
     result = np.round(result, 5)
     return result
 
@@ -94,38 +95,38 @@ def Leerfile(filename):
     return texto_s
 
 
-## main
-def main():
-    print("\n\n--------------- PROYECTO CRIPTOGRAFÍA ---------------")
+# ## main
+# def main():
+#     print("\n\n--------------- PROYECTO CRIPTOGRAFÍA ---------------")
    
-    # filename=input("\n--> Ingrese el nombre del archivo .txt a encriptar: ")
-    # omessage=(Leerfile(filename)).upper()
+#     # filename=input("\n--> Ingrese el nombre del archivo .txt a encriptar: ")
+#     # omessage=(Leerfile(filename)).upper()
 
-    ## Pedir el mensaje
-    ## .upper por que el diccionario esta todo en mayusculas y hace mas facil la comparacion.
-    omessage=(input("\n--> Ingrese el mensaje a encriptar: ")).upper()
+#     ## Pedir el mensaje
+#     ## .upper por que el diccionario esta todo en mayusculas y hace mas facil la comparacion.
+#     omessage=(input("\n--> Ingrese el mensaje a encriptar: ")).upper()
 
-    ## Convertir el mensaje a matriz
-    ToMatrix = toMatriz(omessage,keysize)
+#     ## Convertir el mensaje a matriz
+#     ToMatrix = toMatriz(omessage,keysize)
  
-    ## Encriptar mensaje
-    encode(ToMatrix, key)
+#     ## Encriptar mensaje
+#     encode(ToMatrix, key)
     
-    ## Imprimir el mensaje encriptado
-    print("\n\n--> Mensaje encriptado:")
-    # toDictionary(encodeMessage)
-    prettyPrint(encodeMessage)
-    print("\n\n")
+#     ## Imprimir el mensaje encriptado
+#     print("\n\n--> Mensaje encriptado:")
+#     # toDictionary(encodeMessage)
+#     prettyPrint(encodeMessage)
+#     print("\n\n")
 
-    message = (input("\n--> Ingrese el mensaje a decodificar: ")).upper()
-    # keyInv = input("\n--> Ingrese la matriz clave: ")
-    keyInv = np.linalg.inv(key) # se deben ingresar
-    keyInvL = len(keyInv)
+#     message = (input("\n--> Ingrese el mensaje a decodificar: ")).upper()
+#     # keyInv = input("\n--> Ingrese la matriz clave: ")
+#     keyInv = np.linalg.inv(key) # se deben ingresar
+#     keyInvL = len(keyInv)
 
-    message = toMatrix(message, keyInvL)
-    toDictionary(decode(message, keyInv))
+#     message = toMatrix(message, keyInvL)
+#     toDictionary(decode(message, keyInv))
 
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+    # main()
--- main.py
+++ main.py
@@ -1,4 +1,66 @@
-import codificar
-import decodificar
+import random
+import numpy as np
+import codificar as cod
 
-codificar.main()
+keysize = random.randint(3,5)
+keyValue = random.randint(3,5)
+key = np.random.randint((keyValue), size=(keysize, keysize))
+
+## main
+def main():
+    print("\n\n--------------- PROYECTO CRIPTOGRAFÍA ---------------")
+    print("\nQué desea hacer?")
+    print("1. Encriptar un archivo .txt ")
+    print("2. Encriptar un mesaje ")
+    print("3. Desencriptar y generar un archivo .txt ")
+    print("4. Desencriptar un mensaje ")
+    
+    seguir = "y"
+
+    while seguir == "y":
+        opcion = int(input("\nIngrese el número de la accion que desea realizar: "))
+        
+        if opcion == 1:
+            filename=input("\n--> Ingrese el nombre del archivo .txt a encriptar: ")
+            omessage = (cod.Leerfile(filename)).upper()   # ingreso por archivo .txt
+
+            ToMatrix = cod.toMatriz(omessage,keysize)
+            encodeMessage = cod.encode(ToMatrix, key)
+            
+            print("\n\n--> Mensaje encriptado:")
+            cod.prettyPrint(encodeMessage)
+            print("\n--> Matriz clave: ")
+            cod.prettyPrint(key)
+            print("\n\n")
+
+        if opcion == 2:
+            omessage = input("\n--> Ingrese el mensaje a encriptar: ").upper() # ingreso manual
+
+            ToMatrix = cod.toMatriz(omessage,keysize)
+            encodeMessage = cod.encode(ToMatrix, key)
+
+            print("\n\n--> Mensaje encriptado:")
+            # toDictionary(encodeMessage)
+            cod.prettyPrint(encodeMessage)
+            print("\n--> Matriz clave: ")
+            cod.prettyPrint(key)
+            print("\n\n")
+        if opcion == 3:
+            pass
+        
+        if opcion == 4:
+            message = (input("\n--> Ingrese el mensaje a decodificar: ")).upper()
+            keyInv = input("\n--> Ingrese la matriz clave: ")
+            # keyInv = np.linalg.inv(key) # se deben ingresar
+
+            keysize = int(input("--> Ingrese el tamaño de la matriz clave: "))
+            message = cod.toMatrix(message, keysize)
+            keyInv = cod.toMatrix(keyInv,keysize)
+            cod.toDictionary(cod.decode(message, keyInv))
+
+        seguir = input("\nDesea realizar otra accion? (y/n) ")
+
+
+
+if __name__ == "__main__":
+    main()
