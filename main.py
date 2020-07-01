d7a95837f016ffbd0b4a26a494995a299e89e52d
AndreaNathalia <49224603+AndreaNathalia@users.noreply.github.com>
2020-06-30 20:09:19 -0600

    changes in main

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def main():
     key = np.random.randint((keyValue), size=(keysize, keysize))
 
     print("\n\n--------------- PROYECTO CRIPTOGRAFÍA ---------------")
-    print("\nQué desea hacer?")
-    print("1. Encriptar un archivo .txt ")
+
+    print("\n1. Encriptar un archivo .txt ")
     print("2. Encriptar un mesaje ")
     print("3. Desencriptar y generar un archivo .txt ")
     print("4. Desencriptar un mensaje ")
@@ -18,48 +18,70 @@ def main():
     seguir = "y"
 
     while seguir == "y":
-        opcion = int(input("\nIngrese el número de la accion que desea realizar: "))
+        opcion = int(input("\n► Ingrese el número correspondiente a la accion que desea realizar: "))
         
         if opcion == 1:
-            filename=input("\n--> Ingrese el nombre del archivo .txt a encriptar: ")
+            print("\n\n////////// ENCRIPTANDO ARCHIVO //////////")
+
+            filename = input("\n► Ingrese el nombre del archivo .txt a encriptar: ")
             omessage = (cod.Leerfile(filename)).upper()   # ingreso por archivo .txt
 
             ToMatrix = cod.toMatriz(omessage, keysize)
             encodeMessage = cod.encode(ToMatrix, key)
             
-            print("\n\n--> Mensaje encriptado:")
+            print("\n\n• Mensaje encriptado:")
             cod.prettyPrint(encodeMessage)
-            print("\n--> Matriz clave: ")
+
+            print("\n\n• Matriz clave: ")
             cod.prettyPrint(key)
-            print("\n--> Tamaño clave:", keysize)
-            print("\n\n")
+
+            print("\n\n• Tamaño clave: \n", keysize)
+
+            print("\n/////////////////////////////////////////")
 
         if opcion == 2:
-            omessage = input("\n--> Ingrese el mensaje a encriptar: ").upper() # ingreso manual
+            print("\n\n////////// ENCRIPTANDO MENSAJE //////////")
+
+            omessage = input("\n► Ingrese el mensaje a encriptar: ").upper() # ingreso manual
 
             ToMatrix = cod.toMatriz(omessage,keysize)
             encodeMessage = cod.encode(ToMatrix, key)
 
-            print("\n\n--> Mensaje encriptado:")
+            print("\n\n• Mensaje encriptado:")
             # toDictionary(encodeMessage)
             cod.prettyPrint(encodeMessage)
-            print("\n--> Matriz clave: ")
+
+            print("\n\n• Matriz clave: ")
             cod.prettyPrint(key)
-            print("\n\n")
+
+            print("\n\n• Tamaño clave: \n", keysize)
+
+            print("\n/////////////////////////////////////////")
+
         if opcion == 3:
-            pass
+            print("\n\n////////// DESENCRIPTADNO //////////")
+
+
+            print("\n/////////////////////////////////////////")
+
         
         if opcion == 4:
-            message = (input("\n--> Ingrese el mensaje a decodificar: ")).upper()
-            keyInv = input("\n--> Ingrese la matriz clave: ")
+            print("\n\n////////// DESENCRIPTADNO //////////")
 
-            keysize = int(input("--> Ingrese el tamaño de la matriz clave: "))
+            message = (input("\n► Ingrese el mensaje a desencriptar: ")).upper()
+            keyInv = input("\n► Ingrese la matriz clave: ")
+            keysize = int(input("\n► Ingrese el tamaño de la matriz clave: "))
             print("")
+
+            print("\n----- Mensaje desencriptado -----\n")
             message = cod.toMatrix(message, keysize)
             keyInv = cod.toMatrix(keyInv,keysize)
             cod.toDictionary(cod.decode(message, keyInv))
+            print("")
 
-        seguir = input("\\nnDesea realizar otra accion? (y/n) ")
+            print("\n/////////////////////////////////////////")
+
+        seguir = input("\n\n► ¿Desea realizar otra accion? (y/n): ")
 
 
 
